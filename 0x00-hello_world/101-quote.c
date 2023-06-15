23f0df471832e33bc408b9f3a5d4241027cc1d4c
ginio102 <agtafau@gmail.com>
2023-06-15 15:47:37 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+ * main - A program that prints with a new line
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful"" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
