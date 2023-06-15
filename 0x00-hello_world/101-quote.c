60eba45a7a95e8114189b2c9499cb44569581b80
ginio102 <agtafau@gmail.com>
2023-06-15 16:47:00 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
 #include<unistd.h>
+#include<stdio.h>
 /**
- * main - A program that prints with a new line
+ * main - Entry point
  * Return: 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful"" - Dora Korpar, 2015-10-19 \n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59");
 	return (1);
 }
