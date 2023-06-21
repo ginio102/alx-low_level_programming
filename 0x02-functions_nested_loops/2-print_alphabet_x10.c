cb0a370dd651c0f1a05b2ce18e4791dc74ea56b2
ginio102 <agtafau@gmail.com>
2023-06-20 18:55:02 -0700

    2-Print Alpabet x 10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - print alphabet 10 times
+ * Return: Always 0.
+*/
+void print_alphabet_x10(void)
+
+{
+	int i;
+	int j;
+
+	for
+		(j = 1; i <= 10; i++);
+	{
+	for
+		(j = 97; j <= 122; i++);
+	{
+		_putchar(j);
+		{
+			_putchar('\n');
+		}
+	}
+	}
+}
