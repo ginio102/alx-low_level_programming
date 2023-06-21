1324456e75679db324e657a2cde002babc1b4d54
ginio102 <agtafau@gmail.com>
2023-06-20 17:59:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<unistd.h>
+/**
+ * main - print alphabet
+ * Return: 0 (Success)
+*/
+void print_alphabet(void)
+
+{
+	int i;
+
+	for (i = 97; i <= 122; i++);
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
