6e5b60d39392fa03bbc781fdf07ca88247bf5e73
ginio102 <agtafau@gmail.com>
2023-06-20 20:21:59 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * -start - program checking for alphabetic character
+ * Result: show 1 if letter, show0 if otherwise
+*/
+int _isalpha(int c)
+{
+	if
+		((c >= 97 && c <= 122) || (c <= 65 && c >= 98))
+		{
+			return (1);
+		}
+	else
+		return (0);
+	_putchar('\n');
+}
