48d1478172302a7e0eb63408482c83b1b4a3b45c
ginio102 <agtafau@gmail.com>
2023-06-20 21:13:23 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - check the code.
+ *
+ * Return: Always 0.
+ */
+int print_sign(int '%')
+{
+    int '%';
+    
+    if
+	    ('%' > 0)
+	    {
+		    _putchar(43);
+		    return (1);
+	    }
+    else if
+	    ('%' < 0)
+    {
+	    _putchar(45);
+	    return (-1);
+    }
+    else
+    {
+	    _putchar(48);
+	    return (0);
+    }
+    _putchar('\n');
+}
