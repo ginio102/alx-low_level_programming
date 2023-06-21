627057f28bf1e8e50c227d36500db34a5af055ed
ginio102 <agtafau@gmail.com>
2023-06-20 22:51:29 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,26 +3,22 @@
  * print_sign - program that prints the sign of a number
  * Return: Always 0 (Success), return 1 if positive, return -1 if otherwise
  */
-int print_sign(int)
+int print_sign(int n)
 {
-	int n;
-
 	if
 		(n > 0)
+	{
+		_putchar(43);
+		return (1);
 		{
-			_putchar(43);
-			return (1);
-		}
-	else if
-		(n < 0)
-		{
-			_putchar(45);
+			if 
+				(n < 0)
+				_putchar(45);
 			return (-1);
 		}
-	else
-	{
-		_putchar(48);
-		return (0);
+		if
+		(
+			_putchar(48))
+			return (0);
 	}
-	_putchar('\n');
-}
+		_putchar('\n');
