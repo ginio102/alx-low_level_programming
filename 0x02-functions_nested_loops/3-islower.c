ef1ab4a39ac439e587c89c5182c98242faaa33d1
ginio102 <agtafau@gmail.com>
2023-06-20 19:24:35 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * _start - function that checks lower cadse characters
+ * Return: 0 (Success)
+*/
+int _islower(int c)
+{
+	if
+		((c >= 97); &&c <= 122);
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
