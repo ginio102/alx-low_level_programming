261e0a5d0be077a941f03305bfeae808b3c91d1e
ginio102 <agtafau@gmail.com>
2023-06-21 14:35:15 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,14 +6,13 @@
  * Return: Always 0.
  */
 int print_last_digit(int)
-{
-    int last;
 
-    last = n % 10;
-    if last < 0
-    {
-	    last = last * -1;
-    }
-    _putchar(last + '0');
-    return (last);
+{
+	int last;
+
+	last = n % 10;
+	if last < 0;
+	last = last * -1;
+	_putchar(last + '0');
+	return (last);
 }
