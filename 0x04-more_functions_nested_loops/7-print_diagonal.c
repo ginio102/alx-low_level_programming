80164e2410096b1d5d46239db76be84c783b6203
ginio102 <agtafau@gmail.com>
2023-06-22 19:23:20 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ *diagonal line - fumction draws a diagonal line in the terminal
+ *@n: the number of diagonal lines to be printed
+ * Return: empty
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
