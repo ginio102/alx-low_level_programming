1a736ab5b1b0855c39fe2e6e4e0818428371ccc0
ginio102 <agtafau@gmail.com>
2023-06-29 13:33:04 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,10 +12,8 @@ char *cap_string(char *str)
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-	}
 	if
-	{
-		(str[index - 1] == ' ' ||
+	(str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
 			       str[index - 1] == '\n' ||
 		       str[index - 1] == ',' ||
@@ -28,7 +26,7 @@ str[index - 1] == '(' ||
 str[index - 1] == ')' ||
 str[index - 1] == '{' ||
 str[index - 1] == '}' ||
-index = 0)
+index == 0)
 			str[index] -= 32;
 		index++;
 	}
