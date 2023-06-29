a39586d692c95d8ceb20cb55043ab217db273981
ginio102 <agtafau@gmail.com>
2023-06-29 10:51:11 -0700

    main.h

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,23 @@
+include "main.h"
+#include<unistd.h>
+#include<stdio.h>
+/**
+ * main - program that prints _putchar and a new line
+ * @ c: print character
+ * On error, -1 is returned, and errno is set appropriately
+ * Return: 0 (Success)
+*/
+int main(void)
+
+{
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+                return (0);
+}
