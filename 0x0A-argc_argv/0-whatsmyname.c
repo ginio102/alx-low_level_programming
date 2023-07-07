f311088b229537217d27c481b0bf6431bdd022a6
ginio102 <agtafau@gmail.com>
2023-07-07 13:32:23 -0700

    vim 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * _main - function that prints its name
+ * @argc: input count
+ * @argv: input vector
+ * Return: Always 0
+ */
+
+int _main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _main(int argc, char *argv[]);
+
+#endif /* MAIN_H*/
