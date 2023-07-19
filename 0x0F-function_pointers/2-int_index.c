3470f2f0661d966733b7091bed7e6e7519ab5f10
ginio102 <agtafau@gmail.com>
2023-07-19 09:05:10 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include"function_pointers.h"
+/**
+ * int_index - interger search
+ * @array: of inputs
+ * @size: of inputs
+ * @cmp: comparison of inputs
+ * Return: Interger
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+				i++;
+			}
+		}
+	}
+	return (-1);
+}
