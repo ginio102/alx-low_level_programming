a239bc42206f59a6d062b8cba46d485614aa501b
ginio102 <agtafau@gmail.com>
2023-07-19 09:58:29 -0700

    3-calc.h

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,22 @@
+#ifndef _HEADER_
+#define _HARDER_
+#include<stddef.h>
+/**
+ * struct op - struct
+ * @op: operator
+ * @f: associated function
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+}op_t;
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+int _putchar(char c);
+
+#endif /*_HEADER_*/
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include"3-calc.h"
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * op_add - adds intergers
+ * @a: input
+ * @b: input
+ * Return: Interger
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts intergers
+ * @a: input
+ * @b: input
+ * Return: Interger
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies intergers
+ * @a: input
+ * @b: input
+ * Return: Interger
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides intergers
+ * @a: input
+ * @b: input
+ * Return: Interger
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - remainder intergers in division
+ * @a: input
+ * @b: input 
+ * Return: remainder of intergers
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
