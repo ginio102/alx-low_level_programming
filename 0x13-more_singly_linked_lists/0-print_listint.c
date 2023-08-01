43a0ecab6f8b3a0a44e95c62600b3102b0b53141
ginio102 <agtafau@gmail.com>
2023-08-01 11:04:11 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,18 @@
 #include"lists.h"
 /**
- * print_listint - all elements of a list
- * @h: list type listint_t to print
+ * print_listint - elements of list
+ * @h: type listint_t to print
  * Return: total nodes
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
-
+	
 	while (h)
 	{
 		printf("%d\n", h->n);
 		num++;
 		h = h->next;
 	}
-
 	return (num);
 }
