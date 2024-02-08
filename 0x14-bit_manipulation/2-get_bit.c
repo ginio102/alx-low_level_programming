beac502c7215dfe3a6a9be1216b2a100cd4ed241
ginio102 <133601943+ginio102@users.noreply.github.com>
2024-02-08 14:43:42 +0200

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * get_bit - prog returns the value of a bit at a given index.
+ * get_bit - prog returns the value of a bit at a given index
  * @n: number string to search for bit
  * @index: index to check for bit
  * Return: bit value, or -1 in error
@@ -11,10 +11,10 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int divisor, check;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
-		return (-1);
+return (-1);
 	divisor = 1 << index;
 	check = n & divisor;
 	if (check == divisor)
-		return (1);
+return (1);
 	return (0);
 }
