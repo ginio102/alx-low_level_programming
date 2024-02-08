aaa14385091092233d74c8aed11750e0448304d2
ginio102 <133601943+ginio102@users.noreply.github.com>
2024-02-08 15:17:40 +0200

    5-flips_bit.c

--- 0x14-bit_manipulation/5-flips_bit.c
+++ 0x14-bit_manipulation/5-flips_bit.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * flip_bits - checks number of bits to change
+ * @n: start number
+ * @m: end number
+ * Return: bits to be changed
+ */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 unsigned int count = 0;
