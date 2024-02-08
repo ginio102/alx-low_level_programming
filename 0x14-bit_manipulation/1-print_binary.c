b47e9897fe1991ca091e133d69b1ea42b0df0a16
ginio102 <133601943+ginio102@users.noreply.github.com>
2024-02-08 14:35:32 +0200

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -16,3 +16,30 @@ unsigned long int _pow(unsigned int base, unsigned int power)
 		num *= base;
 	return (num);
 }
+/**
+ * print_binary - prints numbers in binary
+ * @n: number
+ * Return: binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int divisor, check;
+	char flag;
+
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
+	{
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
+	}
+}
